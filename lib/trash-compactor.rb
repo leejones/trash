971a0650bcdb71bb313a62711d86c6568688517c
Lee Jones <scribblethink@gmail.com>
2010-02-26 23:41:09 -0500

    Initial specs and build out

---
+++ lib/trash-compactor.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+
+class Trash
+  def self.compact!(*arguments)
+    
+  end
+  
+  def self.has_trashcan?
+    File.directory? "#{ENV['HOME']}/.Trash" 
+  end
+  
+  def self.can(file_paths)
+    file_paths.split(' ').each do |file_path|
+      file_name = File.basename(file_path)
+      file_extension = File.extname(file_path)
+      if (File.exists?("#{ENV['HOME']}/.Trash/#{file_name}"))
+        new_file_name = file_name.gsub(file_extension, "01#{file_extension}")
+      else
+        new_file_name = file_name
+      end
+      FileUtils.mv(File.expand_path(file_path), "#{ENV['HOME']}/.Trash/#{new_file_name}")
+    end
+  end
+end
