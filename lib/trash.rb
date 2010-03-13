645a2a277c8f57aeedf208cd7d3ced2624e569f7
Lee Jones <scribblethink@gmail.com>
2010-03-13 10:14:50 -0500

    Added initialize method to create a trashcan if one does not exist Changed class methods to instance methods

--- lib/trash.rb
+++ lib/trash.rb
@@ -1,11 +1,18 @@
 require 'fileutils'
 
 class Trash
-  def self.has_trashcan?
-    File.directory? "#{ENV['HOME']}/.Trash" 
+  attr_accessor :trashcan
+  
+  def initialize(options = {})
+    @trashcan = options[:trashcan].nil? ? "#{ENV['HOME']}/.Trash" : options[:trashcan]
+    create_trashcan_if_absent
+  end
+
+  def has_trashcan?
+    File.directory? @trashcan 
   end
   
-  def self.throw_out(paths)
+  def throw_out(paths)
     paths.split(' ').each do |path|
       path = File.expand_path(path)
       FileUtils.mv(path, "#{ENV['HOME']}/.Trash/#{unique_file_name(path)}")
@@ -16,7 +23,11 @@ class Trash
   
   private
   
-  def self.unique_file_name(path)
+  def create_trashcan_if_absent
+    FileUtils.mkdir_p @trashcan unless has_trashcan?
+  end
+  
+  def unique_file_name(path)
     path_name = File.basename(path)
     path_extension = File.extname(path)
     if (File.exists?("#{ENV['HOME']}/.Trash/#{path_name}"))
--- spec/trash_spec.rb
+++ spec/trash_spec.rb
@@ -41,13 +41,13 @@ describe "Trash" do
   end
 
   it "moves a file to the trash" do
-    Trash.throw_out("/tmp/testing.txt")
+    Trash.new.throw_out("/tmp/testing.txt")
     tmp_should_not_contain "testing.txt"
     trash_should_contain "testing.txt"
   end
   
   it "moves multiple files to the trash" do
-    Trash.throw_out("/tmp/testing.txt /tmp/testing2.txt")
+    Trash.new.throw_out("/tmp/testing.txt /tmp/testing2.txt")
     tmp_should_not_contain "testing.txt"
     tmp_should_not_contain "testing2.txt"
     trash_should_contain "testing.txt"
@@ -55,28 +55,28 @@ describe "Trash" do
   end
 
   it "appends a number to the filename if a file with same name already exisits in trash" do
-    Trash.throw_out("/tmp/testing.txt")
+    Trash.new.throw_out("/tmp/testing.txt")
     tmp_should_not_contain "testing.txt"
     trash_should_contain "testing.txt"
     original = File.new("/Users/leejones/.Trash/testing.txt", "r")
     original.read.should == "default text\n"
     
     `echo 'testing different file with same name' > /tmp/testing.txt`
-    Trash.throw_out("/tmp/testing.txt")
+    Trash.new.throw_out("/tmp/testing.txt")
     tmp_should_not_contain "testing.txt"
     trash_should_contain "testing01.txt" 
     third = File.new("/Users/leejones/.Trash/testing01.txt", "r")
     third.read.should == "testing different file with same name\n"
   
     `echo 'testing different file 2 with same name' > /tmp/testing.txt`
-    Trash.throw_out("/tmp/testing.txt")
+    Trash.new.throw_out("/tmp/testing.txt")
     tmp_should_not_contain "testing.txt"
     trash_should_contain "testing02.txt"
     fourth = File.new("/Users/leejones/.Trash/testing02.txt", "r")
     fourth.read.should == "testing different file 2 with same name\n"
 
     `echo 'testing different file 3 with same name' > /tmp/testing.txt`
-    Trash.throw_out("/tmp/testing.txt")
+    Trash.new.throw_out("/tmp/testing.txt")
     tmp_should_not_contain "testing.txt"
     trash_should_contain "testing03.txt"
     fifth = File.new("/Users/leejones/.Trash/testing03.txt", "r")
@@ -85,21 +85,34 @@ describe "Trash" do
   
   it "moves a directory to the trash" do
     dir = `mkdir -p /tmp/testdir01`
-    Trash.throw_out("/tmp/testdir01")
+    Trash.new.throw_out("/tmp/testdir01")
     tmp_should_not_contain "testdir01"
     trash_should_contain_directory "testdir01"
   end
 
   it "moves multiple directories to the trash" do
     dirs = `mkdir -p /tmp/testdir01 /tmp/testdir02`
-    Trash.throw_out("/tmp/testdir01 /tmp/testdir02")
+    Trash.new.throw_out("/tmp/testdir01 /tmp/testdir02")
     tmp_should_not_contain "testdir01"
     tmp_should_not_contain "testdir02"
     trash_should_contain_directory "testdir01"
     trash_should_contain_directory "testdir02"
   end
 
-  it "finds the trashcan" do
-    Trash.has_trashcan?.should == true
+  describe "trashcan" do
+    it "finds the trashcan" do
+      FileUtils.mkdir_p "#{ENV['HOME']}/.Trash"
+      Trash.new.has_trashcan?.should == true
+    end
+
+    it "creates a trashcan if one does not exist" do
+      delete("/tmp/test_trash_can")
+      File.exist?("/tmp/test_trash_can").should == false
+      oscar = Trash.new({:trashcan => "/tmp/test_trash_can"})
+      File.exist?("/tmp/test_trash_can").should == true
+      File.directory?("/tmp/test_trash_can").should == true
+      delete("/tmp/test_trash_can")
+    end
   end
+
 end
