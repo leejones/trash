3a98d59068215e3caf04e5da6e10be7c279d4092
Eric Mathison <dev@ekmathison.com>
2013-09-14 21:18:39 -0700

    Update RSpec to version 2.14

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'trash'
-require 'spec'
-require 'spec/autorun'
+require 'rspec/autorun'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   
 end
