de133d0a422e545bd607303dff7d122c01cb4cb4
Brian Davis <gbriandavis@fastmail.com>
2023-08-03 11:25:37 -0700

    use `exist?`, not `exists?`

--- lib/trash.rb
+++ lib/trash.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 class Trash
   attr_reader :errors
   attr_accessor :trash_can
-  
+
   def initialize(options = {:trash_can => "#{ENV['HOME']}/.Trash"})
     @trash_can = options[:trash_can]
     create_trash_can_if_absent
@@ -11,9 +11,9 @@ class Trash
   end
 
   def has_trash_can?
-    File.directory? @trash_can 
+    File.directory? @trash_can
   end
-  
+
   def throw_out(*paths)
     paths.each do |path|
       path = File.expand_path(path)
@@ -24,25 +24,25 @@ class Trash
         return 1
       end
     end
-    
+
     return 0
   end
-  
+
   def add_error(error)
     @errors << error
   end
-  
+
   private
-  
+
   def create_trash_can_if_absent
     FileUtils.mkdir_p(@trash_can) unless has_trash_can?
   end
-  
+
   def unique_file_name(path)
     file_name      = File.basename(path)
     file_extension = File.extname(path)
 
-    return file_name unless File.exists?("#{@trash_can}/#{file_name}")
+    return file_name unless File.exist?("#{@trash_can}/#{file_name}")
 
     if File.directory? path
       unique_file_name_finder { |c| "#{file_name}#{"%02d" % c}" }
@@ -50,13 +50,13 @@ class Trash
       unique_file_name_finder { |c| "#{file_name.gsub(file_extension, "#{"%02d" % c}#{file_extension}")}" }
     end
   end
-  
+
   def unique_file_name_finder
     count = 1
-    while File.exists?("#{@trash_can}/#{yield(count)}")
+    while File.exist?("#{@trash_can}/#{yield(count)}")
       count += 1
     end
     return yield(count)
   end
-  
+
 end
